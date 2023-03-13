9be075dd7a51a9cfe84184168272c8f21aec25da
Yuuuumoooo <yumo.22@intl.zju.edu.cn>
2023-03-13 18:05:54 +0800

    create Practical5

---
+++ Practical5/variables.py
@@ -0,0 +1,18 @@
+a=-3.19
+b=-118.24
+c=116.39
+d=a-b
+e=c-a
+if d>e: 
+    print("Rob travel further to Los Angeles")
+elif d==e:
+    print("Rob travel equally to Los Angeles and Haining")
+else: 
+    print("Rob travel further to Haining")
+#Rob travel further to Haining
+X=4<5
+Y=4>5
+W=X and Y
+Z=X or Y
+print(W,Z)
+#False True
