177959718d14154245d63f44f51e6f4eb5784dc9
Yuuuumoooo <yumo.22@intl.zju.edu.cn>
2023-03-20 19:46:01 +0800

    Olympic Costs

---
+++ Practical6/Olympic_Costs.py
@@ -0,0 +1,15 @@
+#store and sort costs
+costs=[1,8,15,7,5,14,43,40]
+costs=sorted(costs)
+print(costs)
+#output: [1, 5, 7, 8, 14, 15, 40, 43]
+
+#draw a bar plot
+import matplotlib.pyplot as plt
+N=8
+color=["orange","lightcoral","palevioletred","orchid","gold","lightblue","orangered","sandybrown"]
+x=["Los Angeles 1984","Sydney 2000","Atlanta 1996","Seoul 1988","Athens 2003","Barcelona 1992","London 2012","Beijing 2008"]
+plt.bar(x,costs,color=color)
+plt.xticks(rotation=20,fontsize=8)
+plt.title("Olympic Costs")
+plt.show()
