87ed3d27383805ade4d08a5a87b06c6b555ea032
Yuuuumoooo <yumo.22@intl.zju.edu.cn>
2023-03-20 20:03:01 +0800

    add ylable

--- Practical6/Olympic_Costs.py
+++ Practical6/Olympic_Costs.py
@@ -6,10 +6,10 @@ print(costs)
 
 #draw a bar plot
 import matplotlib.pyplot as plt
-N=8
 color=["orange","lightcoral","palevioletred","orchid","gold","lightblue","orangered","sandybrown"]
 x=["Los Angeles 1984","Sydney 2000","Atlanta 1996","Seoul 1988","Athens 2003","Barcelona 1992","London 2012","Beijing 2008"]
 plt.bar(x,costs,color=color)
 plt.xticks(rotation=20,fontsize=8)
+plt.ylabel("Costs(in $ billions)")
 plt.title("Olympic Costs")
 plt.show()
