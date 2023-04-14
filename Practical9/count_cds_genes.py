356d281b7aab121bc4f9514ff1f52077cac0c20e
Yuuuumoooo <yumo.22@intl.zju.edu.cn>
2023-04-14 18:44:47 +0800

    only select the last3

--- Practical9/count_cds_genes.py
+++ Practical9/count_cds_genes.py
@@ -1,23 +1,28 @@
 import re
 
 #ask for an input and create a file
-stop=input("TAA/TAG/TGA:")
-new_file=open(stop+"_stop_genes.fa","w")
-file=open("xid-1055764_1")
+stop=input("TAA/TAG/TGA:")  #ask for a stop input
+stop_codon=["TAA","TAG","TGA"]
+file=open("xid-1055764_1") #open sequence file
 
-sequence=""
-for line in file:
-  if not re.search(">",line):
-    new_line=re.sub(r"(.+)$\n",r"\1",line)
-    sequence+=new_line
-  else:
-    if re.findall(r"ATG.+"+stop,sequence):
-      number=str(sequence.count(stop))
-      new_name =" ".join(map(str,new_name))+" total number "+number+"\n"
-                        #change to str type
-      sequence+="\n\n"
-      new_file.write(new_name)
-      new_file.write(sequence)
-    sequence=""
-    new_name = re.findall(r"^>\S+",line)
-new_file.close()
+if stop in stop_codon:
+  new_file=open(stop+"_stop_genes.fa","w") #create a new file
+  sequence=""
+  for line in file:
+    if not re.search(">",line):  #sequence
+      new_line=re.sub(r"(.+)$\n",r"\1",line)
+      sequence+=new_line
+    else:  #gene name
+      if re.findall(r"ATG.+"+stop+"$",sequence):
+        number=str(sequence.count(stop))
+        new_name =" ".join(map(str,new_name))+" total number "+number+"\n"
+                         #change to str type
+        sequence+="\n\n"
+        new_file.write(new_name)
+        new_file.write(sequence)
+      sequence=""
+      new_name = re.findall(r"^>\S+",line)
+  new_file.close()
+  file.close()
+else: 
+  print("input a wrong stop condon")
--- Practical9/get_TGA_stops.py
+++ Practical9/get_TGA_stops.py
@@ -2,21 +2,22 @@ import re
 import os
 
 #extract all genes with TGA stop codon
-os.chdir("/Users/qiuliyan/Desktop/IBI/IBI1_2022-23/Practical9")
-file=open("xid-1055764_1")
-new_file=open("TGA_genes.fa","w")
+os.chdir("/Users/qiuliyan/Desktop/IBI/IBI1_2022-23/Practical9")  #change directory
+file=open("xid-1055764_1")  #open file
+new_file=open("TGA_genes.fa","w")  #create new file
 
 sequence="" 
 for line in file:
-  if not re.search(">",line):
-    new_line=re.sub(r"(.+)$\n",r"\1",line)
+  if not re.search(">",line):  #sequence
+    new_line=re.sub(r"(.+)$\n",r"\1",line)  #put the sequence into one variable without \n
     sequence+=new_line
-  else:
-    if re.findall(r"ATG.+TGA",sequence):
+  else:  #gene name
+    if re.findall(r"ATG.+TGA$",sequence):
       new_name =" ".join(map(str,new_name))+"\n"  #change to str type
       sequence+="\n"
       new_file.write(new_name)
       new_file.write(sequence)
     sequence=""
     new_name = re.findall(r"^>\S+",line)
+file.close()
 new_file.close()
