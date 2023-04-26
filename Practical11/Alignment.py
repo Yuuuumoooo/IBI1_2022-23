8e3cb77ca3befb220ee30d43773c6e2417d1b687
Yuuuumoooo <yumo.22@intl.zju.edu.cn>
2023-04-26 23:51:15 +0800

    correct the method

--- Practical11/Alignment.py
+++ Practical11/Alignment.py
@@ -1,43 +1,65 @@
 #import nessary packages
-from Bio import pairwise2
-from Bio.Align import substitution_matrices
 import re
+import pandas as pd
 
 #import three protein sequences
-species=["human","mouse","cat"]
-pro={}
-for i in species:
-  file=open("ACE2_"+i+".fa","r")
-  seq=file.read()
-  pro[i]=re.sub(r".+\n(.+)\n",r"\1",seq)
-  file.close()
+seq=open("ACE2_human.fa","r").read()
+human=re.sub(r".+\n(.+)\n",r"\1",seq)
 
-#reference:https://biopython.org/docs/1.75/api/Bio.pairwise2.html
-#import BLOSUM62 matrix
-matrix=substitution_matrices.load("BLOSUM62")
+seq=open("ACE2_mouse.fa","r").read()
+mouse=re.sub(r".+\n(.+)\n",r"\1",seq)
 
-#alignment
-human_mouse=pairwise2.align.globaldx(pro["human"],pro["mouse"],matrix)
-score1=human_mouse[0][2]
-hm=pairwise2.align.globalxx(pro["human"],pro["mouse"])
-print(hm[0][2])
-perc1=hm[0][2]/len(pro["human"])
+seq=open("ACE2_cat.fa","r").read()
+cat=re.sub(r".+\n(.+)\n",r"\1",seq)
 
-human_cat=pairwise2.align.globaldx(pro["human"],pro["cat"],matrix)
-score2=human_cat[0][2]
-hc=pairwise2.align.globalxx(pro["human"],pro["cat"])
-perc2=hc[0][2]/len(pro["human"])
+#import BLOSUM62
+matrix=pd.read_excel(r"BLOSUM62.xlsx",index_col=0)
 
-mouse_cat=pairwise2.align.globaldx(pro["mouse"],pro["cat"],matrix)
-score3=mouse_cat[0][2]
-mc=pairwise2.align.globalxx(pro["mouse"],pro["cat"])
-perc3=mc[0][2]/len(pro["mouse"])
+#compare human and mouse
+score=0
+for i in range(len(human)):
+  col=human[i]
+  raw=mouse[i]
+  score+=matrix.loc[col,raw]
+print("seq1=human\nseq2=mouse\nBLOSUM62 score=",score)
 
-#print the findings
-print("human and mouse have score",score1,"in protein sequence length of",len(pro["human"]),"with overlap percentage of",perc1,"\n",human_mouse[0][1])
-print("human and cat have score",score2,"in protein sequence length of",len(pro["human"]),"with overlap percentage of",perc2,"\n",human_cat[0][1])
-print("mouse and cat have score",score3,"in protein sequence length of",len(pro["mouse"]),"with overlap percentage of",perc3,"\n",mouse_cat[0][1])
+same=0
+for i in range(len(human)):
+  if human[i]==mouse[i]:
+    same+=1
+per=same/len(human)*100
+print("the identical aa percentage is",per,"%\n")
+  
+#compare human and cat
+score=0
+for i in range(len(human)):
+  col=human[i]
+  raw=cat[i]
+  score+=matrix.loc[col,raw]
+print("seq1=human\nseq2=cat\nBLOSUM62 score=",score)
+    
+same=0
+for i in range(len(human)):
+  if human[i]==cat[i]:
+    same+=1
+per=same/len(human)*100
+print("the identical aa percentage is",per,"%\n")
+
+#compare mouse and cat
+score=0
+for i in range(len(mouse)):
+  col=mouse[i]
+  raw=cat[i]
+  score+=matrix.loc[col,raw]
+print("seq1=mouse\nseq2=cat\nBLOSUM62 score=",score)
+
+same=0
+for i in range(len(mouse)):
+  if mouse[i]==cat[i]:
+    same+=1
+per=same/len(mouse)*100
+print("the identical aa percentage is",per,"%\n")
 
 #my interpretation
-#Because human_cat has the largest score, the sequences of human and cat are most closely related.
-#Human, mouse and cat are all closely related, probably because they share a common ancestor and they all evolved into mammals.
+#Because human-cat has the largest score, the sequences of human and cat are most closely related.
+#Human, mouse and cat are all closely related (>80%), probably because they share a common ancestor and they all evolved into mammals.
